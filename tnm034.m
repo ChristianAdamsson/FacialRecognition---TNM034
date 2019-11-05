dac90159406a52e565bf529a0ef941ec79e64a9f
Christian Adamsson <Christian.liu171@gmail.com>
2019-11-05 13:06:38 +0100

    ändrat till precis som i sliden

--- tnm034.m
+++ tnm034.m
@@ -1,9 +1,15 @@
 function id = tnm034(im)
 
 %
-% im = image 
+% im = image of unknown face, RGB-image in uint8 format in
+% the range [0,255]
 %
-%
+% id: The identity number(integer) of the identified person,
+% i.e. '1', '2', ... , '16' for the persons belonging to 'db1'
+% and '0' for all other faces
 %
 %
 id = 0;
+% Your program code:
+%%%%%%%%%%%%%%%%%%%%%%%%
+
