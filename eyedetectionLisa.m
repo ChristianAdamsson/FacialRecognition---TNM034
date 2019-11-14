e7ae4e4711d1eeba465f6bf2f686a274ab2801c9
Lisa Gren <lisa.c.gren@gmail.com>
2019-11-14 10:06:52 +0100

    updated eyedetectionLisa

--- eyedetectionLisa.m
+++ eyedetectionLisa.m
@@ -1,13 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Läser in en RGB bild
+% Konverterar till double
+% Konverterar till YCbCr
+%
+% Skapar EyeMap för chrominance och luminance enligt metod föreslagen i
+% FACE DETECTION IN COLOR IMAGES
+% Rein-Lien Hsu et al.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 clc
 close all
 clear all
 
 imRGB = imread('image_0009.jpg');
 imRGB=im2double(imRGB);
-% whos imRGB;
 imYCbCr = rgb2ycbcr(imRGB);
-whos imYCBCR;
-% imshow(imYCbCr)
 
 %% Isolate Y Cb and Cr
 Y = imYCbCr(:,:,1);
@@ -15,115 +24,87 @@ Cb = imYCbCr(:,:,2);
 Cr= imYCbCr(:,:,3);
 
 figure
-subplot(1,3,1)
+subplot(2,3,1)
 imshow(Y);
-subplot(1,3,2)
+subplot(2,3,2)
 imshow(Cb);
-subplot(1,3,3)
+subplot(2,3,3)
 imshow(Cr);
-
-% highestPixelValue = max(Cr(:));
-% lowestPixelValue = min(Cr(:));
-
 
 %% normalize channels
 Y = imadjust(Y,stretchlim(Y),[0 1]);
 Cb = imadjust(Cb,stretchlim(Cb),[0 1]);
 Cr = imadjust(Cr,stretchlim(Cr),[0 1]);
 
-
-figure
-subplot(1,3,1)
+subplot(2,3,4)
 imshow(Y);
-subplot(1,3,2)
+subplot(2,3,5)
 imshow(Cb);
-subplot(1,3,3)
+subplot(2,3,6)
 imshow(Cr);
 
-% %%  convert to 0 255 uint8
-% Y = 255*Y;
-% Y = uint8(Y);
-% 
-% Cb = 255*Cb;
-% Cb = uint8(Cb);
-% 
-% Cr = 255*Cr;
-% Cr = uint8(Cr);
-% 
-% % highestPixelValue = max(Y(:));
-% % lowestPixelValue = min(Y(:));
-% 
-% figure
-% subplot(1,3,1)
-% imshow(Y);
-% subplot(1,3,2)
-% imshow(Cb);
-% subplot(1,3,3)
-% imshow(Cr);
+%% cb2
+
+Cb2 = Cb.^2;
+Cb2 = imadjust(Cb2,stretchlim(Cb2),[0 1]);
+
+figure
+subplot(4,2,1)
+imshow(Cb);
+subplot(4,2,2)
+imshow(Cb2);
 
 
-%% calculate components of illumination map
-% Shall they be [0 1] or [0 255] before square??
-% https://books.google.se/books?id=r02WDwAAQBAJ&pg=PA1174&lpg=PA1174&dq=cb+cr+illumination+eye+map&source=bl&ots=2NoIrTyql4&sig=ACfU3U2jFRGC0biFNv-DMJw2u_OkIz7zhQ&hl=sv&sa=X&ved=2ahUKEwjX7_nOrOflAhUNxqYKHYLWCzUQ6AEwEnoECA4QBA#v=onepage&q=cb%20cr%20illumination%20eye%20map&f=false
+%% cr2
+Cr2 = (Cr-1).^2;
+Cr2 = imadjust(Cr2,stretchlim(Cr2),[0 1]);
 
+subplot(4,2,3)
+imshow(Cr);
+subplot(4,2,4)
+imshow(Cr2);
 
+%% CbCr
 
-% Cb2 = Cb.^2;
-% % Cb2 = double(Cb2);
-% Cb2 = imadjust(Cb2,stretchlim(Cb2),[0 1]);
-% Cb2 = 255*Cb2;
-% Cb2 = uint8(Cb2);
-% 
-% figure
-% subplot(1,2,1)
-% imshow(Cb);
-% subplot(1,2,2)
-% imshow(Cb2);
-% 
-% Cr2 = Cr.^2;
-% Cr2 = imadjust(Cr2,stretchlim(Cr2),[0 1]);
-% Cr2 = 255*Cr2;
-% Cr2 = uint8(Cr2);
-% 
-% figure
-% subplot(1,2,1)
-% imshow(Cr);
-% subplot(1,2,2)
-% imshow(Cr2);
-
-
-%% Temp values for map thing
-Cb2 = Cb;
-Cr2 = 1-Cr;
 CbCr = Cb./Cr;
+subplot(4,2,6)
+imshow(CbCr)
 
 %% Eye Map C
 
-EyeMapC = (Cb2 + Cr2 + CbCr)/3;
-figure
-imshow(EyeMapC);
-
+EyeMapC = (Cb2 + Cr2 + CbCr)./3;
+subplot(4,2,8)
+imshow(EyeMapC)
 
 %% Eye Map L
 
 % structuring element
-%se = offsetstrel('ball',5,5);
-%Yuint = uint8(Y*255);
-se1 = strel('line',3,0);
-se2 = strel('line',3,90);
+% Different se for dilate and erode?
+% Also, should be hemisphere, not disk!
+% se = offsetstrel('ball',1,1);
+se = strel('disk',10); 
 
-dilatedY = imdilate(Y,[se1 se2],'full');
+dilatedY = imdilate(Y,se);
 figure
-subplot(1,3,1)
+subplot(3,3,1)
 imshow(dilatedY)
 
-erodedY = imerode(Y,[se1 se2],'full');
-subplot(1,3,2)
+erodedY = imerode(Y,se);
+subplot(3,3,2)
 imshow(erodedY)
 
-subplot(1,3,3)
-EyeMapL = dilatedY./erodedY;
+EyeMapL = dilatedY./(erodedY +1 );
+subplot(3,3,3)
 imshow(EyeMapL)
+
+subplot(3,3,6)
+imshow(EyeMapC)
+
+%% Combine final eye map
+
+EyeMap = EyeMapC.*EyeMapL; 
+subplot(3,3,9)
+imshow(EyeMap)
 
 
 
