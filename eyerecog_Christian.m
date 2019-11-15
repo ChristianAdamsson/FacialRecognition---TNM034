2a5c74a2064f5cd34350cd24c08bf6af9c90d0ff
Christian Adamsson <Christian.liu171@gmail.com>
2019-11-15 11:25:57 +0100

    trying to implement everything together

--- eyerecog_Christian.m
+++ eyerecog_Christian.m
@@ -6,6 +6,38 @@ im = imread('image_0009.jpg');
 I = rgb2gray(im);
 figure
 imshow(I)
+%% Elins implementation
+img = imread('image_0009.jpg');
+
+
+S = sum(img,3);
+[~,idx] = max(S(:));
+[row,col] = ind2sub(size(S),idx); %Hitta ljusaste punkten
+
+
+%imshow(img);
+viscircles([col, row], 3, 'Color', 'b');
+
+hsvImg = rgb2hsv(img);
+
+%imshow(hsvImg);
+
+thresholdLogical = hsvImg(:, :, 1) > 0 & hsvImg(:, :, 1) < 50 & hsvImg(:,:,2) > 0.23 & hsvImg(:,:,2) < 0.68;
+
+resultHsvImg = thresholdLogical.*hsvImg(:,:,3);
+
+resultLogical = resultHsvImg > 0.6;
+
+%imshow(resultLogical);
+
+skinImg = img.*uint8(resultLogical);
+
+%imshow(skinImg);
+
+sMin = 0.23;
+sMax = 0.68;
+hMin = 0;
+hMax = 50;
 
 %% Run this section for Color-based method 
 [counts,binLocations] = imhist(I);
@@ -25,8 +57,8 @@ SE = strel('disk',r,n);
 
 binaryImage = imopen(newim, SE);
 
-figure;
-imshow(binaryImage);
+%figure;
+%imshow(binaryImage);
 
 
 %% https://se.mathworks.com/matlabcentral/fileexchange/25157-image-segmentation-tutorial
@@ -54,8 +86,8 @@ J2 = imdilate(J2, SE3);
 j2 = imerode(J2, SE4);
 J2 = imerode(J2, SE2);
 J2 = imerode(J2, SE4);
-figure;
-imshow(J2);
+%figure;
+%imshow(J2);
 
 %% försöker skapa hybrid
 
@@ -70,7 +102,7 @@ for i = size(J2, 2)
     end
 end
 
-    imshow(hybridim)
+    %imshow(hybridim)
 
 
 
@@ -100,58 +132,58 @@ Y = imYCbCr(:,:,1);
 Cb = imYCbCr(:,:,2);
 Cr= imYCbCr(:,:,3);
 
-figure
-subplot(2,3,1)
-imshow(Y);
-subplot(2,3,2)
-imshow(Cb);
-subplot(2,3,3)
-imshow(Cr);
+%figure
+%subplot(2,3,1)
+%imshow(Y);
+%subplot(2,3,2)
+%imshow(Cb);
+%subplot(2,3,3)
+%imshow(Cr);
 
 %% normalize channels
 Y = imadjust(Y,stretchlim(Y),[0 1]);
 Cb = imadjust(Cb,stretchlim(Cb),[0 1]);
 Cr = imadjust(Cr,stretchlim(Cr),[0 1]);
 
-subplot(2,3,4)
-imshow(Y);
-subplot(2,3,5)
-imshow(Cb);
-subplot(2,3,6)
-imshow(Cr);
+%subplot(2,3,4)
+%imshow(Y);
+%subplot(2,3,5)
+%imshow(Cb);
+%subplot(2,3,6)
+%imshow(Cr);
 
 %% cb2
 
 Cb2 = Cb.^2;
 Cb2 = imadjust(Cb2,stretchlim(Cb2),[0 1]);
 
-figure
-subplot(4,2,1)
-imshow(Cb);
-subplot(4,2,2)
-imshow(Cb2);
+%figure
+%subplot(4,2,1)
+%imshow(Cb);
+%subplot(4,2,2)
+%imshow(Cb2);
 
 
 %% cr2
 Cr2 = (Cr-1).^2;
 Cr2 = imadjust(Cr2,stretchlim(Cr2),[0 1]);
 
-subplot(4,2,3)
-imshow(Cr);
-subplot(4,2,4)
-imshow(Cr2);
+%subplot(4,2,3)
+%imshow(Cr);
+%subplot(4,2,4)
+%imshow(Cr2);
 
 %% CbCr
 
 CbCr = Cb./Cr;
-subplot(4,2,6)
-imshow(CbCr)
+%subplot(4,2,6)
+%imshow(CbCr)
 
 %% Eye Map C
 
 EyeMapC = (Cb2 + Cr2 + CbCr)./3;
-subplot(4,2,8)
-imshow(EyeMapC)
+%subplot(4,2,8)
+%imshow(EyeMapC)
 
 %% Eye Map L
 
@@ -162,38 +194,60 @@ imshow(EyeMapC)
 se = strel('disk',10); 
 
 dilatedY = imdilate(Y,se);
-figure
-subplot(3,3,1)
-imshow(dilatedY)
+%figure
+%subplot(3,3,1)
+%imshow(dilatedY)
 
 erodedY = imerode(Y,se);
-subplot(3,3,2)
-imshow(erodedY)
+%subplot(3,3,2)
+%imshow(erodedY)
 
 EyeMapL = dilatedY./(erodedY +1 );
-subplot(3,3,3)
-imshow(EyeMapL)
+%subplot(3,3,3)
+%imshow(EyeMapL)
 
 subplot(3,3,6)
-imshow(EyeMapC)
+%imshow(EyeMapC)
 
 %% Combine final eye map
 
 EyeMap = EyeMapC.*EyeMapL; 
-subplot(3,3,9)
-imshow(EyeMap)
+%subplot(3,3,9)
+%imshow(EyeMap)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TODO:
+% Fix dilation and erosion of EyeMapL
+% Dilate and threshold the final EyeMap
+% 
+% TODO: Implement the detection
+% From FACE DETECTION IN COLOR IMAGES, Rein-Lien Hsu et al.
+% "Eye candidates are selected by using 
+% (i) pyramid decomposition of the dilated eye map for coarse localizations 
+% and (ii) binary morphological closing and iterative thresholding 
+% on this dilated map for fine localizations.
+% 
+% TODO: Implement verification
+% "The eyes and mouth candidates are verified by checking 
+% (i) luma variations of eye and mouth blobs; 
+% (ii) geometry and orientation constraints of eyes-mouth triangles; 
+% and (iii) the presence of a face boundary around eyes-mouth triangles." 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 %% försök nmr 2 skapa hybrid
-ImageIlluCol = EyeMap .* binaryImage;
-ImageColEdge = binaryImage .* J2; 
+ImageIlluCol = EyeMap .* binaryImage + resultLogical;
+ImageColEdge = binaryImage .* J2 + resultLogical; 
 ImageIlluEdge =  EyeMap.*J2;
+
+ImageIlluEdge = ImageIlluEdge + resultLogical;
 figure
 imshow(ImageColEdge)
 figure
 imshow(ImageIlluCol)
 figure
 imshow(ImageIlluEdge)
+
 
 
 
