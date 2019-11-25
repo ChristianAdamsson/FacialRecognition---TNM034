98756a76737d461ec6135b3af16d63f57e93e5ab
Christian Adamsson <Christian.liu171@gmail.com>
2019-11-25 23:35:17 +0100

    did some scaling and bound boxing on images, think it works

--- scaling_Christian.m
+++ scaling_Christian.m
@@ -1,7 +1,12 @@
 id = 0;
 
-im = imread('image_0009.jpg');
-img = imread('image_0009.jpg');
+%im = imread('image_0009.jpg');
+%img = imread('image_0009.jpg');
+%im = imread('db1_15.jpg');
+%img = imread('db1_15.jpg');
+%måste ha ny elins kod för att db1_10 ska funka.
+im = imread('db1_04.jpg');
+img = imread('db1_04.jpg');
 %tnm034(im);
 %figure
 %imshow(im);
@@ -43,7 +48,6 @@ newimage = img .* uint8(resultLogical);
 
 boundbox = regionprops(resultLogical,'Area', 'BoundingBox');
 boundbox(1).BoundingBox(1)
-%%plot(centroids(:,1),centroids(:,2),'b*')
 %[left, top, width, height
 left = floor(boundbox(1).BoundingBox(1))
 top = floor(boundbox(1).BoundingBox(2))
@@ -53,16 +57,38 @@ height = boundbox(1).BoundingBox(4)
 %mybox = zeros(width, height);
 %mybox(1) = left: left + width;
 %testbox = [left: (left + width), 0:top];
-scale = 1/2;
-J = imresize(newimage,scale);
-intbox = uint8(boundbox(1).BoundingBox);
-%bbox = [int(left), int(top), int(width), int(height)];
-bboxResized = bboxresize(intbox,scale);
+
+scale = 1;
+J = imresize(newimage,1);
+%nothing working :///
+
+
+%Things that didn't work
+%bboxA = floor(boundbox(1).BoundingBox);
+%bboxA = int64(bboxA);
+%bboxB = bboxresize(bboxA ,scale);
+%intbox = int8(boundbox(1).BoundingBox);
+
+% In order to see the box around the selected face. 
+J = insertObjectAnnotation(J,'Rectangle',boundbox(1).BoundingBox,'testbox');
 
 %testbox = imrezise(newimage, [testbox(1), testbox(2)]);
 figure;
-imshow(newimage);
+imshow(J);
 
 
+% cropping the image to the bounding box. 
+rezisedimg = imcrop(J, boundbox(1).BoundingBox);
+%rezisedimg = J(left: left + width, top: top + height);
+
+%showing the cropped image
+figure;
+imshow(rezisedimg)
+
+%% scaling
+%ska det verkligen skalas varje gång??
+scaledim = imresize(rezisedimg, [400, 300]);
+figure;
+imshow(scaledim);
 
 
